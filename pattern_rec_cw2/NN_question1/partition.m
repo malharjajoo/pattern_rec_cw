b66f4d33724d593e71f074d01a5fbada09e3fa3c
Malhar <malhar.jajoo14@imperial.ac.uk>
2017-12-18 20:39:21 +0000

    Minor tidy up.

--- pattern_rec_cw2/NN_question1/partition.m
+++ pattern_rec_cw2/NN_question1/partition.m
@@ -1,14 +1,12 @@
-function [train_set,train_label,test_set,test_label] = partition(filename)
+function [train_set,train_labels,test_set,test_labels] = partition(filename)
 % Read input csv data file
 data = dlmread(filename);
 
 
-% random indices are used to select training set 
-% remaining indices ( All indices - random indices ) are used to
-% select the validation set.
 train_idx = find(data(:,1)==1);
 test_idx = find(data(:,1)==2);
 
+% extract training/test and sample/label into X and Y.
 X = data(train_idx,:);
 Y = data(test_idx,:);
 
@@ -17,7 +15,7 @@ Y = data(test_idx,:);
 train_set = X(:,3:end);
 test_set = Y(:,3:end);
 
-train_label = X(:,2);
-test_label = Y(:,2);
+train_labels = X(:,2);
+test_labels = Y(:,2);
 
 end
