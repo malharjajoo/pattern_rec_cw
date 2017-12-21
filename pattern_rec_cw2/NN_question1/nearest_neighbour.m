ccd5beb14bc48750c75da098710e3a2b430d892a
Malhar <malhar.jajoo14@imperial.ac.uk>
2017-12-21 16:03:50 +0000

    Updated code for finding best and worst metrics. Also removed hamming metric.

--- pattern_rec_cw2/NN_question1/nearest_neighbour.m
+++ pattern_rec_cw2/NN_question1/nearest_neighbour.m
@@ -1,10 +1,4 @@
-function [best_metric, lowest_class_error, best_accuracy] = nearest_neighbour(train_set,train_label,test_set,test_label)
-
-    % M_train = size(train_set,1); %rows in train data
-    % N_train = size(train_set,2); %cols in train data
-    % 
-    % M_test = size(test_set,1);  %rows in test data
-    % N_test = size(test_set,2);  %cols in test data
+function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train_label,test_set,test_label)
 
     knn_method =  'exhaustive'; %'kdtree';
     
@@ -12,11 +6,14 @@ function [best_metric, lowest_class_error, best_accuracy] = nearest_neighbour(tr
     % 9 metrics
     if(strcmp(knn_method,'exhaustive'))
         metrics_nameList = ["cityblock","chebychev","correlation",...
-                         "cosine","euclidean","hamming"...
+                         "cosine","euclidean"...
                          "jaccard","mahalanobis","minkowski","spearman"];
                      
                          
     else 
+        % since matlab only allows following distance metrics with kdtree method.
+        % for now, kdtree cannot be used with this dataset since attributes > 10.
+        % however, for some other data ....
         metrics_nameList = ["cityblock","chebychev","euclidean","minkowski"];
     end
     
@@ -25,19 +22,14 @@ function [best_metric, lowest_class_error, best_accuracy] = nearest_neighbour(tr
                      
     % row contains prediction labels for each metric.
     % coloumn corresponds to metric.
-    % dimension is 60x<number of metrics> for now....
+    % dimension is 60x<number of metrics> for this test set....
     metric_prediction_labels = zeros(size(test_set,1), size(metrics_nameList,2));            
 
     % k-value for each neighbour
-    k_values = 1:3:30;
+    k_values = 1:3:30; % 1:3:118 --> get very bad results. Curse of high dimensions ?
     Error_list = ones(size(metrics_nameList,2),size(k_values,2));
 
-		% find best accuracy among all metrics for a given k.
-    best_accuracy = ones(1,size(k_values,2));
-    best_metric = string(k_values);  % probably a bad way of initializing this .. TODO:change later
-    
-    
-    
+    % ========== Testing Phase ( There is NO training phase! ) ========
     % For each k
         % For each distance metric, 
             % Perform nearest neighbour classification. Store prediction labels.
@@ -48,38 +40,63 @@ function [best_metric, lowest_class_error, best_accuracy] = nearest_neighbour(tr
    
             metric_name = char(metrics_nameList(i));
         
+            
             Mdl = fitcknn(train_set,train_label,'NumNeighbors',k_values(j),'Standardize',1,...
                     'Distance',metric_name,'NSMethod',knn_method) ; 
-
-            [label,score,cost] = predict(Mdl,test_set);
+           
+            
+            tic;
+            [label,~,~] = predict(Mdl,test_set);
+            toc;
             metric_prediction_labels(:,i) = label;
             
            
         end
         
-        % Find binary classification error for all metrics for a given k.
-        binary_class_error = findClassificationError(metric_prediction_labels,test_label);
-        Error_list(:,j) = binary_class_error;
-        
+        % Find binary classification error for all metric for a given k.
+        Error_list(:,j)  = findClassificationError(metric_prediction_labels,test_label);
+
     end
     
-    % For each k, find metric with lowest classification error
-    % and save results. The find the metric 
-   
+    
+        
+    % For a given "k", store best accuracy found and which metric it corresponds to.
+    highest_accuracy_k = ones(1,size(k_values,2));
+    % probably a bad way of initializing this .. TODO:change later
+    best_metric_k = string(k_values);
+    
+    
+     % For a given "k", store best accuracy found and which metric it corresponds to.
+    lowest_accuracy_k = ones(1,size(k_values,2));
+    % probably a bad way of initializing this .. TODO:change later
+    worst_metric_k = string(k_values);
+      
+    % For each k, find metric with lowest(best) and highest(worst) classification error
+    % and save results.
     for i = 1:size(k_values,2)
     
-        binary_class_error = Error_list(:,i);
-        [lowest_class_error,idx] = min(binary_class_error);
-        best_metric(i) = metrics_nameList(idx);
-        best_accuracy(i) = (1-lowest_class_error)*100;
+        [lowest_error_k,idx] = min(Error_list(:,i));
+        best_metric_k(i) = metrics_nameList(idx);
+        highest_accuracy_k(i) = (1-lowest_error_k)*100;
+        
+        [highest_error_k,idx] = max(Error_list(:,i));
+        worst_metric_k(i) = metrics_nameList(idx);
+        lowest_accuracy_k(i) = (1-highest_error_k)*100;
     end
-    [final_accuracy,idx] = max(best_accuracy);
-    final_metric = best_metric(idx);
-    fprintf(' (Best Metric, Best accuracy) = (%s,%f) found at k = %d',final_metric,final_accuracy,k_values(idx));
+    
+    [best_accuracy,idx1] = max(highest_accuracy_k);
+     best_metric = best_metric_k(idx1);
+    
+     [worst_accuracy,idx2] = min(lowest_accuracy_k);
+     worst_metric = worst_metric_k(idx2);
+    
+    fprintf(' (Best Metric, Best accuracy) = (%s,%f) found at k = %d\n',best_metric,best_accuracy,k_values(idx1));
+    fprintf(' (Worst Metric, Worst accuracy) = (%s,%f) found at k = %d\n',worst_metric,worst_accuracy,k_values(idx2));
     % ====== Plot all results ==========
     
     % plot (Binary) classification error ?
     plotGraph(k_values,metrics_nameList,Error_list);
+    
 end
 
 
