943fb81316fdf6720b294e4b9e500f32b08be1c7
Malhar <malhar.jajoo14@imperial.ac.uk>
2017-12-22 00:51:43 +0000

    Added code for chi_square metric. Best result so far. Updated images with chi_square metric plot

--- pattern_rec_cw2/NN_question1/nearest_neighbour.m
+++ pattern_rec_cw2/NN_question1/nearest_neighbour.m
@@ -4,17 +4,16 @@ function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train
     
     metrics_nameList = [];
     % 9 metrics
+  
     if(strcmp(knn_method,'exhaustive'))
         metrics_nameList = ["cityblock","chebychev","correlation",...
-                         "cosine","euclidean"...
-                         "jaccard","mahalanobis","minkowski","spearman"];
-                     
-                         
+                         "cosine","euclidean","chi_square",...
+                         "jaccard","mahalanobis","minkowski"];                
     else 
         % since matlab only allows following distance metrics with kdtree method.
         % for now, kdtree cannot be used with this dataset since attributes > 10.
         % however, for some other data ....
-        metrics_nameList = ["cityblock","chebychev","euclidean","minkowski"];
+        metrics_nameList = ["cityblock","euclidean","minkowski"];
     end
     
 
@@ -26,7 +25,7 @@ function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train
     metric_prediction_labels = zeros(size(test_set,1), size(metrics_nameList,2));            
 
     % k-value for each neighbour
-    k_values = 1:3:30; % 1:3:118 --> get very bad results. Curse of high dimensions ?
+    k_values =  1:3:118; 
     Error_list = ones(size(metrics_nameList,2),size(k_values,2));
 
     % ========== Testing Phase ( There is NO training phase! ) ========
@@ -35,19 +34,32 @@ function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train
             % Perform nearest neighbour classification. Store prediction labels.
             % Find the classification error on TEST set.
             % Choose distance metric with least classification error.
+    w = [1;1; 1; 1; 1; 1; 1; 0.01; 1; 1; 0.01 ;1 ; 10];
+    chiSqrDist = @(x,Z,wt)sqrt((bsxfun(@minus,x,Z).^2)*wt);
+    
+    Md1 = [];
     for j = 1:size(k_values,2)
         for i = 1:size(metrics_nameList,2)
    
-            metric_name = char(metrics_nameList(i));
-        
+            if( strcmp(metrics_nameList(i),"chi_square"))
+                 metric_name = @(x,Z)chiSqrDist(x,Z,w) ;
+            else
+                 metric_name = char(metrics_nameList(i));
+            end
             
-            Mdl = fitcknn(train_set,train_label,'NumNeighbors',k_values(j),'Standardize',1,...
-                    'Distance',metric_name,'NSMethod',knn_method) ; 
-           
             
-            tic;
+            if( strcmp(metrics_nameList(i),"minkowski"))
+                Mdl = fitcknn(train_set,train_label,'NumNeighbors',k_values(j),'Standardize',1,...
+                    'Distance',metric_name,'DistanceWeight','equal','NSMethod',knn_method,'Exponent',3) ; 
+                
+            else
+                Mdl = fitcknn(train_set,train_label,'NumNeighbors',k_values(j),'Standardize',1,...
+                    'Distance',metric_name,'DistanceWeight','equal','NSMethod',knn_method) ; 
+            end
+            
+            % tic;
             [label,~,~] = predict(Mdl,test_set);
-            toc;
+            % toc;
             metric_prediction_labels(:,i) = label;
             
            
@@ -58,8 +70,6 @@ function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train
 
     end
     
-    
-        
     % For a given "k", store best accuracy found and which metric it corresponds to.
     highest_accuracy_k = ones(1,size(k_values,2));
     % probably a bad way of initializing this .. TODO:change later
@@ -83,8 +93,9 @@ function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train
         worst_metric_k(i) = metrics_nameList(idx);
         lowest_accuracy_k(i) = (1-highest_error_k)*100;
     end
+   
     
-    [best_accuracy,idx1] = max(highest_accuracy_k);
+     [best_accuracy,idx1] = max(highest_accuracy_k);
      best_metric = best_metric_k(idx1);
     
      [worst_accuracy,idx2] = min(lowest_accuracy_k);
@@ -93,7 +104,7 @@ function [best_metric_k, highest_accuracy_k] = nearest_neighbour(train_set,train
     fprintf(' (Best Metric, Best accuracy) = (%s,%f) found at k = %d\n',best_metric,best_accuracy,k_values(idx1));
     fprintf(' (Worst Metric, Worst accuracy) = (%s,%f) found at k = %d\n',worst_metric,worst_accuracy,k_values(idx2));
     % ====== Plot all results ==========
-    
+
     % plot (Binary) classification error ?
     plotGraph(k_values,metrics_nameList,Error_list);
     
@@ -103,6 +114,19 @@ end
 
 % ================= Helper functions ===========================
 
+
+function [] = displayBestFiveResults(accuracy_k,metric_k,k_values)
+    
+     % Get 5 best metrics ... across all k 
+     [s_accuracy,originalpos] = sort( accuracy_k, 'descend' );
+     n = s_accuracy(1:5);
+     p = originalpos(1:5) ;
+     
+     for i = p
+          fprintf(' (Best Metric, Best accuracy) = (%s,%f) found at k = %d\n',metric_k(i),n(i),k_values(i));
+     end
+end
+    
 function [binary_class_error] = findClassificationError(metric_prediction_labels,test_label)
 
     total_metrics = size(metric_prediction_labels,2);
@@ -131,39 +155,6 @@ end
 
 
 
-% Creates a scatterplot with each metric as series plot
-% For each value of k.
-function plotGraph_Scatter(k_values,metrics_nameList,Error_list)
-    
-    total_metrics = size(metrics_nameList,2);
-    Legend = cell(total_metrics,1);
-    
-    for j = 1:size(k_values,2)
-        k_val = k_values(j);
-        
-        for i = 1:total_metrics
-            scatter(k_val,Error_list(i,j));
-            
-            Legend{i} = char(metrics_nameList(i)) ;
-        
-            % used to create series plot.
-            hold on 
-        end
-        
-    end
-    
-    
-    
-    %yticks([[0:0.2:1],[2:10:70]]);
-    xticks(k_values);
-    xlabel('k values');
-    ylabel('binary classification error');
-    title('Classification Error for distance metrics');
-    legend(Legend);
-    
-end
-
-
 
 % Creates a normal plot with each metric as series plot
 % For each value of k.
@@ -176,7 +167,12 @@ function plotGraph(k_values,metrics_nameList,Error_list)
         for i = 1:total_metrics
             plot(k_values,Error_list(i,:));
             
-            Legend{i} = char(metrics_nameList(i)) ;
+           if( strcmp(metrics_nameList(i),"cityblock"))
+                Legend{i} = 'manhattan' ;
+                
+            else
+                 Legend{i} = char(metrics_nameList(i)) ;
+           end
         
             % used to create series plot.
             hold on 
@@ -184,10 +180,10 @@ function plotGraph(k_values,metrics_nameList,Error_list)
         
     
     %yticks([[0:0.2:1],[2:10:70]]);
-    xticks(k_values);
-    xlabel('k values');
-    ylabel('binary classification error');
-    title('Classification Error for distance metrics');
+    
+    xlabel('no. of nearest neighbours(k)');
+    ylabel('Test (classification) error/100');
+    title('Test Error for distance metrics');
     legend(Legend);
     
 end
